d97884f8209f6e1a6937a97debd7fd75ab71ebf2
TrauDat <34256871+TrauDat@users.noreply.github.com>
2018-05-06 19:45:47 +0700

    Bai 34 - Tich hop CK Finder

--- TrauDatShop.Web/app/components/products/productAddController.js
+++ TrauDatShop.Web/app/components/products/productAddController.js
@@ -31,7 +31,13 @@
                 console.log('Cannot get list parent');
             });
         }
-
+        $scope.ChooseImage = function () {
+            var finder = new CKFinder();
+            finder.selectActionFunction = function (fileUrl) {
+                $scope.product.Image = fileUrl;
+            }
+            finder.popup();
+        }
         loadProductCategories();
     }
 
