7f78eae4310d7d9db2b434cf0f0c8fdd7a8ac683
TrauDat <34256871+TrauDat@users.noreply.github.com>
2018-05-14 21:58:19 +0700

    Bai 45

--- TrauDatShop.Service/ProductService.cs
+++ TrauDatShop.Service/ProductService.cs
@@ -22,6 +22,7 @@ namespace TrauDatShop.Service
 
         IEnumerable<Product> GetLastest(int top);
         IEnumerable<Product> GetHotProduct(int top);
+        IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, string sort, out int totalRow); 
 
         Product GetById(int id);
 
@@ -136,6 +137,30 @@ namespace TrauDatShop.Service
         public IEnumerable<Product> GetHotProduct(int top)
         {
             return _productRepository.GetMulti(x => x.Status && x.HotFlag == true).OrderByDescending(x => x.CreatedDate).Take(top);
+        }
+
+        public IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize,string sort, out int totalRow )
+        {
+            var query = _productRepository.GetMulti(x => x.Status && x.CategoryID == categoryId);
+            switch(sort)
+            {
+             
+                case "popular":
+                    query = query.OrderByDescending(x => x.ViewCount);
+                    break;
+                case "discount":
+                    query = query.OrderByDescending(x => x.PromotionPrice.HasValue);
+                    break;
+                case "price":
+                    query = query.OrderBy(x => x.Price);
+                    break;
+                default:
+                    query = query.OrderByDescending(x => x.CreatedDate);
+                    break;
+            }
+            totalRow = query.Count();
+
+            return query.Skip((page - 1) * pageSize).Take(pageSize);
         }
     }
 }
--- TrauDatShop.Web/Controllers/ProductController.cs
+++ TrauDatShop.Web/Controllers/ProductController.cs
@@ -1,21 +1,50 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrauDatShop.Common;
+using TrauDatShop.Model.Models;
+using TrauDatShop.Service;
+using TrauDatShop.Web.Infrastructure.Core;
+using TrauDatShop.Web.Models;
 
 namespace TrauDatShop.Web.Controllers
 {
     public class ProductController : Controller
     {
+        IProductService _productService;
+        IProductCategoryService _productCategoryService;
+        public ProductController(IProductService productService, IProductCategoryService productCategoryService)
+        {
+            this._productService = productService;
+            this._productCategoryService = productCategoryService;
+        }
         // GET: Product
         public ActionResult Detail(int id)
         {
+           
             return View();
         }
-        public ActionResult Category(int id)
+        public ActionResult Category(int id,int page = 1, string sort ="")
         {
-            return View();
+            int pageSize = int.Parse(ConfigHelper.GetByKey("PageSize"));
+            int totalRow = 0;
+            var productModel = _productService.GetListProductByCategoryIdPaging(id, page, pageSize, sort, out totalRow);
+            var productViewModel = Mapper.Map<IEnumerable<Product>,IEnumerable<ProductViewModel>>(productModel);
+            int totalPage = (int)Math.Ceiling((double)totalRow / pageSize);
+            var category = _productCategoryService.GetById(id);
+            ViewBag.Category = Mapper.Map<ProductCategory, ProductCategoryViewModel>(category);
+            var paginationSet = new PaginationSet<ProductViewModel>()
+            {
+                Items = productViewModel,
+                MaxPage = int.Parse(ConfigHelper.GetByKey("MaxPage")),
+                Page = page,
+                TotalCount = totalRow,
+                TotalPages = totalPage
+            };
+            return View(paginationSet);
         }
     }
 }
